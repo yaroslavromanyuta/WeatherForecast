f7cec2b82aa8d41ee999cfbf6c380de9c5dcb170
yaroslavromanyuta <yaroslav.romanyuta@gmail.com>
2016-05-25 17:35:59 +0300

    make structure

---
+++ app/src/main/java/yaroslavromanyuta/com/ua/weatherforecast/apiInstruments/JsonParser.java
@@ -0,0 +1,7 @@
+package yaroslavromanyuta.com.ua.weatherforecast.apiInstruments;
+
+/**
+ * Created by Yaroslav on 25.05.2016.
+ */
+public class JsonParser {
+}
---
+++ app/src/main/java/yaroslavromanyuta/com/ua/weatherforecast/forecast/greenDaoModel/ForecastResponce.java
@@ -0,0 +1,41 @@
+package yaroslavromanyuta.com.ua.weatherforecast.forecast.greenDaoModel;
+
+import java.util.List;
+
+/**
+ * Created by Yaroslav on 25.05.2016.
+ */
+public class ForecastResponce {
+
+    private City city;
+    private  List<Weather> weather;
+
+    public ForecastResponce(){}
+
+    public ForecastResponce (City city){
+        this.city = city;
+
+        weather = city.getForecast();
+    }
+
+    public ForecastResponce(City city, List<Weather> weather){
+        this.weather = weather;
+        this.city = city;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public List<Weather> getWeather() {
+        return weather;
+    }
+
+    public void setWeather(List<Weather> weather) {
+        this.weather = weather;
+    }
+}
